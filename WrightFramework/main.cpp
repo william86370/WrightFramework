41ceb7ec965cfd80f7352a3b629e85c28259f38a
William Wright <william86370@gmail.com>
2019-02-13 14:54:05 -0500

    Initial Commit

---
+++ WrightFramework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WrightFramework
+//
+//  Created by william wright on 2/13/19.
+//  Copyright © 2019 Silvertap. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
